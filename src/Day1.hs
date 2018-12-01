cb7a88d6733458ac670a9b6578649a8c4e7d847b
Matthew Farrell <farrellm@alum.mit.edu>
2018-12-01 00:43:09 -0500

    day 1 complete

---
+++ src/Day1.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
+
+module Day1 () where
+
+import AdventPrelude
+import qualified Data.Set as S
+
+input :: IO Text
+input = readFile "input-1.txt"
+
+p1 :: IO ()
+p1 = do
+  ls <- lines <$> input
+  let ds = fmap readInt ls
+  print (sum ds)
+
+go :: Set Int -> Int -> Either Int (Set Int)
+go s v =
+  if v `S.member` s
+    then Left v
+    else pure (v `S.insert` s)
+
+p2 :: IO ()
+p2 = do
+  ls <- lines <$> input
+  let ds = fmap readInt ls
+      ss = scanl (+) 0 (cycle ds)
+      s = foldM go S.empty ss
+  print s
